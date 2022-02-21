71faf5924168d485978a8b924aef6422f187837c
Ivan Nikolsky <enty8080@gmail.com>
2022-02-21 22:46:47 +0300

    Create shell_bind_tcp.py

---
+++ hatloads/payloads/linux/armle/shell_bind_tcp.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2022 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from hatasm import HatAsm
+from hatvenom import HatVenom
+
+from hatloads.words import Words
+
+
+class ShellBindTCP(HatAsm, HatVenom, Words):
+    def generate(self, assemble=True, options={}):
+        if 'BPORT' not in options:
+            return b''
+
+        if 'SHELL' in options:
+            shell = options['SHELL'].encode()
+        else:
+            shell = self.shell['sh'].encode()
+
+        shell = shell[::-1]
+        bport = self.convert_port(options['BPORT'])
+
+        shellcode = f"""
+        """
+
+        if assemble:
+            return self.assemble('armle', shellcode)
+        return shellcode
